07e15e16a303f4def620006cde5f53808f6ef2e8
luckyy14 <42903815+luckyy14@users.noreply.github.com>
2023-06-15 18:24:14 +0530

    Revert "First"
    This reverts commit 8fe052ce445611c875f9b5d8fb99b1f0fe575971.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+
+
+
 import React,{ useEffect, useState } from "react";
 import PokemonThumbnail from "./Components/PokemonThumbnail";
 
@@ -22,8 +25,8 @@ function App() {
   }
   useEffect(()=>{
     getAllPokemons()
-  },[]);
-  
+  },[])
+
   return (
     <div className="app-container">
      <h1>Pokemon Kingdom .</h1>
