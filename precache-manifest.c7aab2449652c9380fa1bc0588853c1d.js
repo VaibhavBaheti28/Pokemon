a2809b2e28ba8057161fbfcb7984431fbb13a2ed
Vaibhav <vaibhavbaheti28@gmail.com>
2023-08-13 01:37:11 +0530

    Updates

---
+++ precache-manifest.c7aab2449652c9380fa1bc0588853c1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc827fe6bb3589a3a0fd",
+    "url": "/Pokemon/static/css/main.cdc8568a.chunk.css"
+  },
+  {
+    "revision": "cc827fe6bb3589a3a0fd",
+    "url": "/Pokemon/static/js/main.cc827fe6.chunk.js"
+  },
+  {
+    "revision": "c1c4f2fba6a8a4c5f840",
+    "url": "/Pokemon/static/js/1.c1c4f2fb.chunk.js"
+  },
+  {
+    "revision": "d15fcab501317f2983dd",
+    "url": "/Pokemon/static/js/runtime~main.d15fcab5.js"
+  },
+  {
+    "revision": "c20e41219881376b486facaad52dbdfe",
+    "url": "/Pokemon/index.html"
+  }
+];
